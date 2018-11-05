8f38da460c3610d1091b1b49470da02799b8fe7f
megane42 <h.kzm.gm@gmail.com>
2018-11-05 20:06:11 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                "transform-react-jsx",
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
