92622425d51d3e5bebc4ee0c580d81d7344f0525
megane42 <h.kzm.gm@gmail.com>
2018-11-06 19:21:49 +0900

    add css and make mock

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ import ReactDom from 'react-dom'
 import CountUp  from 'react-countup'
 
 ReactDom.render(
-  <CountUp end={100} />,
-  document.getElementById('counter')
+  <CountUp end={50} />,
+  document.getElementById('counter-number')
 );
